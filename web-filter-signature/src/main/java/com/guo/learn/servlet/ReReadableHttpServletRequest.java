084a60914c8176d60f21489ce913b03868fe234e
link <1352906758@qq.com>
2023-06-21 19:29:10 +0800

    filter修改请求httpservletrequest

---
+++ web-filter-signature/src/main/java/com/guo/learn/servlet/ReReadableHttpServletRequest.java
@@ -0,0 +1,67 @@
+package com.guo.learn.servlet;
+
+import jakarta.servlet.ReadListener;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * 对HttpServletRequest进行读取时，只能读取一次。如果Filter调用getInputStream()读取了一次数据，后续Servlet处理时，再次读取，将无法读到任何数据。怎么办？
+ *
+ * 这个时候，我们需要一个“伪造”的HttpServletRequest，具体做法是使用代理模式，对getInputStream()和getReader()返回一个新的流：
+ */
+public class ReReadableHttpServletRequest extends HttpServletRequestWrapper {
+    private final byte[] body;
+    private boolean open = false;
+
+    public ReReadableHttpServletRequest(HttpServletRequest request, byte[] body) {
+        super(request);
+        this.body = body;
+    }
+
+    // 返回InputStream:
+    public ServletInputStream getInputStream() throws IOException {
+        if (open) {
+            throw new IllegalStateException("Cannot re-open input stream!");
+        }
+        open = true;
+        return new ServletInputStream() {
+            private int offset = 0;
+
+            public boolean isFinished() {
+                return offset >= body.length;
+            }
+
+            public boolean isReady() {
+                return true;
+            }
+
+            public void setReadListener(ReadListener listener) {
+            }
+
+            public int read() throws IOException {
+                if (offset >= body.length) {
+                    return -1;
+                }
+                int n = body[offset] & 0xff;
+                offset++;
+                return n;
+            }
+        };
+    }
+
+    // 返回Reader:
+    public BufferedReader getReader() throws IOException {
+        if (open) {
+            throw new IllegalStateException("Cannot re-open reader!");
+        }
+        open = true;
+        return new BufferedReader(new InputStreamReader(new ByteArrayInputStream(body), StandardCharsets.UTF_8));
+    }
+}
