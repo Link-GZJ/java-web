c7d3437c905fb8cf8bd9c4c8fd28c110e2fb0cbb
link <1352906758@qq.com>
2023-06-27 22:26:59 +0800

    使用aop避坑指南

--- spring-aop-field/src/main/java/com/guo/learn/AppConfig.java
+++ spring-aop-field/src/main/java/com/guo/learn/AppConfig.java
@@ -10,6 +10,11 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @ComponentScan
 @EnableAspectJAutoProxy
 public class AppConfig {
+    /*
+    因此，正确使用AOP，我们需要一个避坑指南：
+    访问被注入的Bean时，总是调用方法而非直接访问字段；
+    编写Bean时，如果可能会被代理，就不要编写public final方法。
+     */
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         MailService bean = context.getBean(MailService.class);
